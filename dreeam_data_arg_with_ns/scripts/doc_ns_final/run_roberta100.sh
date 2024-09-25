da4e2eb7be08ae12d2209793db63d93bdd7fd1e1
vicky <jiazixia@bigai.ai>
2024-09-25 11:17:28 +0800

    config

--- dreeam_data_arg_with_ns/scripts/doc_ns_final/run_roberta100.sh
+++ dreeam_data_arg_with_ns/scripts/doc_ns_final/run_roberta100.sh
@@ -8,15 +8,15 @@ do
   NAME=${TYPE}_datarate${RATE}_value_ns${ns_rate}
    
   python run.py --do_train --add_recall --add_value \
-  --data_dir /scratch/nlp/jiazixia/rl_enhanced/dreeam_pos_neg_rl/dataset/docred \
+  --data_dir data/ \
   --transformer_type roberta \
-  --load_value_path /scratch/nlp/jiazixia/rl_enhanced/dreeam/docred_output/sup_ns_final/docred_roberta_lambda0.05_ns10_rate100/2024-01-24_13:51:57.143670 \
-  --model_name_or_path /scratch/nlp/jiazixia/dreeam/bert_models/roberta-large \
+  --load_value_path ckpt/sup_ns_final/docred_roberta_lambda0.05_ns10_rate100/2024-01-24_13:51:57.143670 \
+  --model_name_or_path bert_models/roberta-large \
   --display_name  ${NAME} \
-  --train_file /scratch/nlp/jiazixia/dreeam/dataset/docred/train_annotated.json \
-  --dev_file /scratch/nlp/jiazixia/dreeam/dataset/docred/dev.json \
-  --test_file /scratch/nlp/jiazixia/rl_enhanced/P3M/dataset/docred/re_docred_test_refine.json \
-  --save_path docred_output/final/${NAME} \
+  --train_file docred/train_annotated.json \
+  --dev_file docred/dev.json \
+  --test_file docred/test_revised.json \
+  --save_path ckpt/docred_output/final/${NAME} \
   --train_batch_size 8 \
   --test_batch_size 8 \
   --gradient_accumulation_steps 1 \
@@ -33,60 +33,5 @@ do
   --seed ${SEED} \
   --num_class 97
 
-  : "
-  python run.py --do_train --add_recall --add_value \
-  --data_dir /scratch/nlp/jiazixia/rl_enhanced/dreeam_pos_neg_rl/dataset/docred \
-  --transformer_type roberta \
-  --load_value_path /scratch/nlp/jiazixia/rl_enhanced/dreeam/redocred_output/sup_ns_final/ns_lambda0.05_ns10_rate10/2023-09-21_18:13:26.844779 \
-  --model_name_or_path /scratch/nlp/jiazixia/dreeam/bert_models/roberta-large \
-  --display_name  ${NAME} \
-  --train_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/redocred/sample_data_for_dreeam/triple_sample_data1/sample_${RATE}_percent_data/train.json \
-  --dev_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/redocred/sample_data_for_dreeam/triple_sample_data1/sample_${RATE}_percent_data/dev.json \
-  --test_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/docred/test_revised.json \
-  --save_path redocred_output/final/${NAME} \
-  --train_batch_size 8 \
-  --test_batch_size 8 \
-  --gradient_accumulation_steps 1 \
-  --num_labels 4 \
-  --lr_transformer 5e-6 \
-  --lr_added 3e-5 \
-  --max_grad_norm 1.0 \
-  --evi_thresh 0.2 \
-  --rl_weight 10.0 \
-  --sample_times 1 \
-  --ns_rate_for_value ${ns_rate} \
-  --evi_lambda ${LAMBDA} \
-  --warmup_ratio 0.06 \
-  --num_train_epochs 30.0 \
-  --seed ${SEED} \
-  --num_class 97
-
-
-  python run.py --do_train --add_recall --add_value \
-  --data_dir /scratch/nlp/jiazixia/rl_enhanced/dreeam_pos_neg_rl/dataset/docred \
-  --transformer_type roberta \
-  --load_value_path /scratch/nlp/jiazixia/rl_enhanced/dreeam/redocred_output/sup_ns_final/ns_lambda0.05_ns10_rate10/2023-09-22_09:09:26.045709 \
-  --model_name_or_path /scratch/nlp/jiazixia/dreeam/bert_models/roberta-large \
-  --display_name  ${NAME} \
-  --train_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/redocred/sample_data_for_dreeam/triple_sample_data2/sample_${RATE}_percent_data/train.json \
-  --dev_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/redocred/sample_data_for_dreeam/triple_sample_data2/sample_${RATE}_percent_data/dev.json \
-  --test_file /scratch/nlp/jiazixia/rl_enhanced/dreeam/dataset/docred/test_revised.json \
-  --save_path redocred_output/final/${NAME} \
-  --train_batch_size 8 \
-  --test_batch_size 8 \
-  --gradient_accumulation_steps 1 \
-  --num_labels 4 \
-  --lr_transformer 5e-6 \
-  --lr_added 3e-5 \
-  --max_grad_norm 1.0 \
-  --evi_thresh 0.2 \
-  --rl_weight 10.0 \
-  --sample_times 1 \
-  --ns_rate_for_value ${ns_rate} \
-  --evi_lambda ${LAMBDA} \
-  --warmup_ratio 0.06 \
-  --num_train_epochs 30.0 \
-  --seed ${SEED} \
-  --num_class 97
-  "
+  
 done
--- dreeam_data_arg_with_ns/scripts/isf_roberta.sh
+++ dreeam_data_arg_with_ns/scripts/isf_roberta.sh
@@ -2,9 +2,9 @@ NAME=$1
 MODEL_DIR=$2
 SPLIT=$3
 
-python run.py --data_dir /scratch/nlp/jiazixia/rl_enhanced/dreeam_pos_neg_rl/dataset/docred \
+python run.py --data_dir data/docred \
 --transformer_type roberta \
---model_name_or_path /scratch/nlp/jiazixia/dreeam/bert_models/roberta-large \
+--model_name_or_path bert_models/roberta-large \
 --display_name  ${NAME} \
 --load_value_path ${MODEL_DIR} \
 --load_path ${MODEL_DIR} \
@@ -15,14 +15,3 @@ python run.py --data_dir /scratch/nlp/jiazixia/rl_enhanced/dreeam_pos_neg_rl/dat
 --num_labels 4 \
 --num_class 97 
 
-#python run.py --data_dir dataset/docred \
-#--transformer_type roberta \
-#--model_name_or_path roberta-large \
-#--display_name  ${NAME} \
-#--load_path ${MODEL_DIR} \
-#--eval_mode fushion \
-#--test_file ${SPLIT}.json \
-#--test_batch_size 8 \
-#--num_labels 4 \
-#--evi_thresh 0.2 \
-#--num_class 97
